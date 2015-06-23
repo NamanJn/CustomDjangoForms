95f034803aaf8f5ef2a2596a64e4cef8593e4918
Naman Jain <nj1411@ic.ac.uk>
2015-06-23 15:22:40 +0100

    first commit

---
+++ myFormFields.py
@@ -0,0 +1,19 @@
+from django import forms
+
+class PositiveIntegerField(forms.IntegerField):
+    def __init__(self, *args, **kwargs):
+
+        minimumvalue = kwargs.pop('min_value', 0)
+        maximumvalue = kwargs.pop("max_value", None)
+        if not kwargs.get('widget'):
+            kwargs['widget'] = forms.NumberInput(attrs={"required":"required"})
+
+        super(PositiveIntegerField, self).__init__(max_value=maximumvalue, min_value=minimumvalue, *args, **kwargs)
+
+class OptionalPositiveIntegerField(PositiveIntegerField):
+    def __init__(self, *args, **kwargs):
+        super(OptionalPositiveIntegerField, self).__init__( required=False, *args, **kwargs)
+
+class OptionalPercentField(OptionalPositiveIntegerField):
+    def __init__(self, *args, **kwargs):
+        super(OptionalPercentField, self).__init__(max_value=100, *args, **kwargs)
